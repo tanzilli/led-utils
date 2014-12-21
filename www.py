927bb60768c589aa656bec9ef933d628fb0567e1
Jürgen Weigert <jnweiger@gmail.com>
2014-12-21 15:35:22 +0100

    Font caching added. We can now scroll much faster and smoother!

--- www.py
+++ www.py
@@ -12,14 +12,17 @@ import thread
 import threading
 
 sliding_message=None
-sliding_delay=0
+sliding_delay=14	# keep in sync with index.html
 red=1<<5
 green=1<<5
 blue=1<<5
+baseline=-1	# vertical text position: -5=top, +4=bottom(no descenders)
 
 class SlidingMessage(threading.Thread):
 	message="Vuoto"
 	stop_request_flag=False
+	invalidate_cache_request_flag=False
+	cache = []
 	
 	def __init__(self,message):
 		threading.Thread.__init__(self)
@@ -31,14 +34,17 @@ class SlidingMessage(threading.Thread):
 
 	def stop(self):
 		self.stop_request_flag=True
-		
-	def run(self):	
+
+	def invalidateCache(self):
+		self.invalidate_cache_request_flag=True
+
+	def run(self):
 		global red,green,blue
 		global sliding_delay
-		
+
 		font1 = ImageFont.truetype('Ubuntu-B.ttf',30)
 
-		im = Image.new("RGB", (32, 32), "black")
+		im = Image.new("RGB", (32, 32), "black")		# The 'off' color
 		draw = ImageDraw.Draw(im)
 		draw.fontmode="1" #No antialias
 		width, height = font1.getsize(self.message)	
@@ -57,30 +63,38 @@ class SlidingMessage(threading.Thread):
 
 			while True:
 				time.sleep(60)
-		
+
+		self.invalidate_cache_request_flag = True
+		cache_idx = 0
 		x = 32
-		while True:
+		while not self.ifStopRequested():
 			x = x - 1
 
 			if x < -(width):
 				x = 32
-				
-			draw.rectangle((0, 0, 31, 31), outline=0, fill=0)
+				cache_idx = 0
 
-			if self.ifStopRequested():
-				break;
+			if self.invalidate_cache_request_flag:
+				self.invalidate_cache_request_flag = False
+				self.cache = [ None for i in range(32 + width + 1) ]
 
-			color_set=(red,green,blue)
-			draw.text((x,-4), self.message, color_set, font=font1)
+			if self.cache[cache_idx] is None:
+				bgcol=(0<<5,0<<5,0<<5)
+				draw.rectangle((0, 0, 31, 31), outline=bgcol, fill=bgcol)	# Background color
 
-			output.truncate(0)
-			im.save(output, format='PPM')
-			buf=output.getvalue()
+				color_set=(red,green,blue)
+				draw.text((x,baseline), self.message, color_set, font=font1)
+
+				output.truncate(0)
+				im.save(output, format='PPM')
+				buf=output.getvalue()
+				self.cache[cache_idx] = buf[13:]
 
 			out_file.seek(0)
-			out_file.write(buf[13:])
+			out_file.write(self.cache[cache_idx])
 
-			time.sleep(sliding_delay/100.0)
+			time.sleep(sliding_delay/1000.0)
+			cache_idx = cache_idx + 1
 
 		out_file.close()
 		return
@@ -96,6 +110,9 @@ class set_color(tornado.web.RequestHandler):
 		green = int(self.get_argument("green", default="0"))<<5
 		blue = int(self.get_argument("blue", default="0"))<<5
 
+		global sliding_message
+		sliding_message.invalidateCache()
+
 class set_sliding_delay(tornado.web.RequestHandler):
 	def get(self):
 		global sliding_delay
@@ -107,7 +124,8 @@ class send_message(tornado.web.RequestHandler):
 
 		message = self.get_argument("message", default="")
 
-		self.write(message)
+		## What is this?
+		# self.write(message)
 		
 		if sliding_message!=None:
 			sliding_message.stop()
@@ -123,7 +141,8 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
+        sliding_message=SlidingMessage(" Welcome ")	# keep in sync with index.html
+        sliding_message.start()
 	application.listen(8000,"0.0.0.0")
 	tornado.ioloop.IOLoop.instance().start() 
-	
 

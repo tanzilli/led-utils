5f850a0a1c7e3476b8c1b3b557b7af5f0ee659a6
Sergio Tanzilli <tanzilli@acmesystems.it>
2015-06-30 00:40:20 +0200

    Changed the thread management

--- www.py
+++ www.py
@@ -10,32 +10,34 @@ from PIL import ImageDraw
 import StringIO
 import thread
 import threading
-
-sliding_message=None
-sliding_delay=0
-red=1
-green=1
-blue=1
+import signal
 
 class SlidingMessage(threading.Thread):
-	message="Vuoto"
+	message="LedPanel"
 	stop_request_flag=False
+	sliding_delay=0
+	red=0
+	green=0
+	blue=1
 	
-	def __init__(self,message):
-		threading.Thread.__init__(self)
-		self.message=message
-		return
-
-	def ifStopRequested(self):
+	def if_stop_requested(self):
 		return self.stop_request_flag
 
 	def stop(self):
 		self.stop_request_flag=True
 		
+	def set_message(self,message):
+		self.message=message
+
+	def set_color(self,red,green,blue):
+		self.red=red
+		self.green=green
+		self.blue=blue
+
+	def set_sliding_delay(self,sliding_delay):
+		self.sliding_delay=sliding_delay
+	
 	def run(self):	
-		global red,green,blue
-		global sliding_delay
-		
 		font1 = ImageFont.truetype('fonts/Ubuntu-B.ttf',30)
 
 		im = Image.new("RGB", (32, 32), "black")
@@ -67,10 +69,10 @@ class SlidingMessage(threading.Thread):
 				
 			draw.rectangle((0, 0, 31, 31), outline=0, fill=0)
 
-			if self.ifStopRequested():
+			if self.if_stop_requested():
 				break;
 
-			color_set=(red,green,blue)
+			color_set=(self.red,self.green,self.blue)
 			draw.text((x,-4), self.message, color_set, font=font1)
 
 			output.truncate(0)
@@ -80,7 +82,7 @@ class SlidingMessage(threading.Thread):
 			out_file.seek(0)
 			out_file.write(buf[13:])
 
-			time.sleep(sliding_delay/100.0)
+			time.sleep(self.sliding_delay/100.0)
 
 		out_file.close()
 		return
@@ -91,29 +93,30 @@ def format_time():
 
 class set_color(tornado.web.RequestHandler):
 	def get(self):
-		global red,green,blue
 		red = int(self.get_argument("red", default="0"))
 		green = int(self.get_argument("green", default="0"))
 		blue = int(self.get_argument("blue", default="0"))
+		t.set_color(red,green,blue)
 
 class set_sliding_delay(tornado.web.RequestHandler):
 	def get(self):
-		global sliding_delay
 		sliding_delay = int(self.get_argument("delay", default="0"))
-
+		t.set_sliding_delay(sliding_delay)
+		
 class send_message(tornado.web.RequestHandler):
 	def get(self):
-		global sliding_message
-
+		global t
 		message = self.get_argument("message", default="")
+		t.set_message(message)
 
-		self.write(message)
-		
-		if sliding_message!=None:
-			sliding_message.stop()
-		
-		sliding_message=SlidingMessage(message)
-		sliding_message.start()
+def signal_handler(signal, frame):
+	global sliding_message
+
+	print 'You pressed Ctrl+C!'
+	t.stop()
+	t.join()
+	sys.exit(0)
+
 
 application = tornado.web.Application([
 	(r"/send_message", send_message),
@@ -122,7 +125,14 @@ application = tornado.web.Application([
 	(r"/(.*)", tornado.web.StaticFileHandler, {"path": ".","default_filename": "index.html"}),
 ])
 
-if __name__ == "__main__":
+t=SlidingMessage()
+
+if __name__ == "__main__":	
+	t.start()
+	
+	signal.signal(signal.SIGINT, signal_handler)
+	print 'Press Ctrl+C to exit'
+	
 	application.listen(80,"0.0.0.0")
 	tornado.ioloop.IOLoop.instance().start() 
 	
